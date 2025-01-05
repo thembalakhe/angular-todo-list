cb5c7b39f6dac82d865ce37c5ca1c72e8abc08a6
thembalakhe <m.themba1738@gmail.com>
2025-01-05 17:21:26 +0200

    try1-qwerty env conf..-refact..2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,13 @@
-
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: '<YOUR_NEW_API_KEY>',
-    // other configurations...
-  }
+  firebaseConfig: {
+  apiKey: "AIzaSyCMOmeS1zX6qhqIcJ5cFIVQoVV_SxUD7Lk",
+  authDomain: "angulartodos-89870.firebaseapp.com",
+  projectId: "angulartodos-89870",
+  storageBucket: "angulartodos-89870.firebasestorage.app",
+  messagingSenderId: "96884917987",
+  appId: "1:96884917987:web:33be1594ef97796d11a7d6",
+  measurementId: "G-CSR0CHLT6L",
+  databaseURL: "https://angulartodos-89870-default-rtdb.europe-west1.firebasedatabase.app/",
+  },
 };
